c21721d04bc571c2597a23a81677e176800b48a8
Nick K <nick.kruchinin@gmail.com>
2022-10-05 01:54:04 +0300

    Add test for pony.create mutation

---
+++ src/server/trpc/route/mutation/pony/create.test.ts
@@ -0,0 +1,25 @@
+import anyTest from "ava"
+
+import type {TestFn} from "ava"
+
+import {withTRPC} from "server/__macro__/withTRPC"
+import {setup, cleanup} from "server/__helper__/database"
+import type {WithTRPCContext} from "server/__macro__/withTRPC"
+
+import {PonyRace} from "server/db/entity/Pony"
+
+const test = anyTest as TestFn<WithTRPCContext>
+
+test.before(setup)
+
+test.after.always(cleanup)
+
+test("Creates a new pony", withTRPC, async (t, trpc) => {
+  const actual = await trpc.mutation("pony.create", {
+    name: "Lyra Heartstrings",
+    race: PonyRace.UNICORN
+  })
+
+  t.is(actual.name, "Lyra Heartstrings")
+  t.is(actual.race, PonyRace.UNICORN)
+})
