f1de1bfe3547be8581d414f2364fc0d3bad0b21f
Nick K <nick.kruchinin@gmail.com>
2022-10-05 03:44:37 +0300

    Setup trpc client and tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,33 @@
+import {QueryClient, QueryClientProvider} from "react-query"
+import {useState, Fragment} from "react"
 import type {AppProps} from "next/app"
 import type {FC} from "react"
 
+import Head from "next/head"
+
+import {trpcClient, TRPCProvider} from "lib/trpc"
+
 import "style/globals.css"
+import "style/tailwind.css"
 
 interface Props extends AppProps { }
 
-const MyApp: FC<Props> = ({Component, pageProps}) => (
-  <Component {...pageProps} />
-)
+const MyApp: FC<Props> = ({Component, pageProps}) => {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Next.js template with Mikro ORM and tRPC</title>
+      </Head>
+
+      <TRPCProvider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </TRPCProvider>
+    </Fragment>
+  )
+}
 
 export default MyApp
