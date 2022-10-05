99bc32aba5accba047d404f47ea7b9b2f72c81a2
Nick K <nick.kruchinin@gmail.com>
2022-10-05 17:06:25 +0300

    Add API route for trpc

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,6 @@
+import {createNextApiHandler} from "@trpc/server/adapters/next"
+
+import {createContext} from "server/trpc/context"
+import {router} from "server/trpc/router"
+
+export default createNextApiHandler({router, createContext})
