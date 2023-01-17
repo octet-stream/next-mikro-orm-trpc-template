107fb01fef60e8ecb67b8b5e91ef280ead03ac0e
Nick K <nick.kruchinin@gmail.com>
2023-01-18 01:21:31 +0300

    Fix issue with enums discovery

--- src/server/db/entity/Pony.ts
+++ src/server/db/entity/Pony.ts
@@ -1,18 +1,13 @@
 import {Entity, Property, Enum, OptionalProps} from "@mikro-orm/core"
+import {isString} from "lodash"
 
 import type {IPonyOutput} from "server/trpc/type/output/PonyOutput"
-// import {PonyRace} from "server/trpc/type/common/PonyRace"
+import {PonyRace} from "server/trpc/type/common/PonyRace"
 
 import type {OptionalDates} from "./BaseDates"
 import {BaseDates} from "./BaseDates"
 
-// FIXME: MikroORM can't discover if enum defined in different file. Deduplicate this enum and find the solution.
-export enum PonyRace {
-  EARTH_PONY = "earth_pony",
-  UNICORN = "unicorn",
-  PEGASUS = "pegasus",
-  ALICORN = "alicorn"
-}
+const races = [...new Set(Object.values(PonyRace).filter(isString))]
 
 @Entity()
 export class Pony extends BaseDates implements IPonyOutput {
@@ -21,9 +16,7 @@ export class Pony extends BaseDates implements IPonyOutput {
   @Property()
   name!: string
 
-  @Enum(() => PonyRace)
+  // ! MikroORM can't discover emun when it declared in separate file. To fix this, we extract values from enum manually
+  @Enum({items: races, type: "string"})
   race!: PonyRace
 }
-
-// Re-export so that mikro-orm can see this type
-// export {PonyRace}
