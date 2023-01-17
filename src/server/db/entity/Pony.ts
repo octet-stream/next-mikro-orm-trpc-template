efd28693ab9ca2648c60dd06a94f8c91aa7757b7
Nick K <nick.kruchinin@gmail.com>
2023-01-18 01:00:14 +0300

    Add temporarily solution for reflect-metadata issue with enums

--- src/server/db/entity/Pony.ts
+++ src/server/db/entity/Pony.ts
@@ -1,10 +1,18 @@
 import {Entity, Property, Enum, OptionalProps} from "@mikro-orm/core"
 
 import type {IPonyOutput} from "server/trpc/type/output/PonyOutput"
-import {PonyRace} from "server/trpc/type/common/PonyRace"
+// import {PonyRace} from "server/trpc/type/common/PonyRace"
 
 import type {OptionalDates} from "./BaseDates"
 import {BaseDates} from "./BaseDates"
+
+// FIXME: MikroORM can't discover if enum defined in different file. Deduplicate this enum and find the solution.
+export enum PonyRace {
+  EARTH_PONY = "earth_pony",
+  UNICORN = "unicorn",
+  PEGASUS = "pegasus",
+  ALICORN = "alicorn"
+}
 
 @Entity()
 export class Pony extends BaseDates implements IPonyOutput {
@@ -13,9 +21,9 @@ export class Pony extends BaseDates implements IPonyOutput {
   @Property()
   name!: string
 
-  @Enum({items: () => PonyRace})
+  @Enum(() => PonyRace)
   race!: PonyRace
 }
 
 // Re-export so that mikro-orm can see this type
-export {PonyRace}
+// export {PonyRace}
