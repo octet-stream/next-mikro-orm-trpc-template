20b20bab3dcd9883b4832276d41467f796b71766
Nick K <nick.kruchinin@gmail.com>
2023-01-29 11:51:52 +0300

    Add PickKeys helper

---
+++ src/lib/type/PickKeys.ts
@@ -0,0 +1,4 @@
+/**
+ * Picks a set of keys from `T` object, and returns respective union type.
+ */
+export type PickKeys<T extends {}, K extends keyof T> = keyof Pick<T, K>
--- src/server/db/entity/BaseDates.ts
+++ src/server/db/entity/BaseDates.ts
@@ -1,22 +1,24 @@
 import {Entity, Property} from "@mikro-orm/core"
 
+import type {PickKeys} from "lib/type/PickKeys"
+
 import type {INodeWithDates} from "server/trpc/type/common/NodeWithDates"
 
 import {Base} from "./Base"
-
-export type OptionalDates = "createdAt" | "updatedAt"
 
 @Entity({abstract: true})
 export abstract class BaseDates extends Base implements INodeWithDates {
   /**
    * Date and time the entity was created
-   */
-  @Property()
-  readonly createdAt: Date = new Date()
+  */
+ @Property()
+ readonly createdAt: Date = new Date()
 
-  /**
-   * Most recent date and time the entity was updated
-   */
-  @Property({onUpdate: () => new Date()})
-  readonly updatedAt: Date = new Date()
+ /**
+  * Most recent date and time the entity was updated
+ */
+@Property({onUpdate: () => new Date()})
+readonly updatedAt: Date = new Date()
 }
+
+export type OptionalDates = PickKeys<BaseDates, "createdAt" | "updatedAt">
