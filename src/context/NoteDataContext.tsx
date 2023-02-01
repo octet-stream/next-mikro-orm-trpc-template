168bc5086a2046ada74b8849a413706d055e8c04
Nick K <nick.kruchinin@gmail.com>
2023-02-01 16:54:57 +0300

    Remove NotesDataContext and NoteDataContext

--- src/context/NoteDataContext.tsx
+++
@@ -1,9 +0,0 @@
-import type {TNoteOutput} from "server/trpc/type/output/NoteOutput"
-
-import {createDataContext} from "lib/context/createDataContext"
-
-export const {
-  Context: NoteDataContext,
-  useDataContext: useNoteDataContext,
-  DataContextProvider: NoteDataContextProvider,
-} = createDataContext<TNoteOutput>()
--- src/context/NotesDataContext.tsx
+++
@@ -1,9 +0,0 @@
-import type {TNotesPageOutput} from "server/trpc/type/output/NotesPageOutput"
-
-import {createDataContext} from "lib/context/createDataContext"
-
-export const {
-  Context: NotesDataContext,
-  useDataContext: useNotesDataContext,
-  DataContextProvider: NotesDataContextProvider,
-} = createDataContext<TNotesPageOutput>()
