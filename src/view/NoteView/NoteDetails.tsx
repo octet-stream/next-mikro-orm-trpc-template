9166c78ad9e31a9e6bbe936850c36060856173fa
Nick K <nick.kruchinin@gmail.com>
2023-02-03 21:40:06 +0300

    Add minimal Markdown support for note details

--- src/view/NoteView/NoteDetails.tsx
+++ src/view/NoteView/NoteDetails.tsx
@@ -1,23 +1,35 @@
+import {createElement, Fragment, useMemo} from "react"
+import {unified} from "unified"
 import type {FC} from "react"
 
+
+import rehypeSanitize from "rehype-sanitize"
+import remarkRehype from "remark-rehype"
+import rehypeReact from "rehype-react"
+import remarkParse from "remark-parse"
+
 import {useNoteStateSnapshot} from "context/NoteStateContext"
+
+import {NoteNoDetails} from "./NoteNoDetails"
+
+const parser = unified()
+  .use(remarkParse)
+  .use(remarkRehype)
+  .use(rehypeSanitize)
+  .use(rehypeReact, {createElement, Fragment})
 
 export const NoteDetails: FC = () => {
   const {details} = useNoteStateSnapshot()
 
+  const parsedDetails = useMemo(
+    () => details ? parser.processSync(details).result : undefined,
+
+    [details]
+  )
+
   return (
     <div className="mt-5">
-      {
-        details ? (
-          <p>
-            {details}
-          </p>
-        ) : (
-          <p className="text-gray-400 dark:text-gray-600">
-            No details
-          </p>
-        )
-      }
+      {parsedDetails ? parsedDetails : <NoteNoDetails />}
     </div>
   )
 }
---
+++ src/view/NoteView/NoteNoDetails.tsx
@@ -0,0 +1,7 @@
+import type {FC} from "react"
+
+export const NoteNoDetails: FC = () => (
+  <p className="text-gray-400 dark:text-gray-600">
+    No details
+  </p>
+)
