e65fc56395e0782c6e8e72ef75350b607e63250b
Nick K <nick.kruchinin@gmail.com>
2023-02-07 08:19:48 +0300

    Fixes for zod types

--- src/server/trpc/type/common/DateTime.ts
+++ src/server/trpc/type/common/DateTime.ts
@@ -1,7 +1,8 @@
 import {parseISO, toDate} from "date-fns"
 import type {infer as Infer} from "zod"
-import {isString} from "lodash"
 import {z} from "zod"
+
+import isString from "lodash/isString"
 
 export const DateTime = z
   .union([z.date(), z.string(), z.number()])
--- src/server/trpc/type/input/CompletionUpdateInput.ts
+++ src/server/trpc/type/input/CompletionUpdateInput.ts
@@ -1,7 +1,11 @@
 import type {infer as Infer} from "zod"
 
+import {Node} from "server/trpc/type/common/Node"
+
 import {CompletionCreateInput} from "./CompletionCreateInput"
 
-export const CompletionUpdateInput = CompletionCreateInput.partial()
+export const CompletionUpdateInput = Node.extend(
+  CompletionCreateInput.partial().shape
+)
 
 export type TCompletionUpdateInput = Infer<typeof CompletionUpdateInput>
