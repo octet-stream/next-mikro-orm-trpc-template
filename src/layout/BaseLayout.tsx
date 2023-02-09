3cff27bf6c20cfa1b4d185773c278e755515f7df
Nick K <nick.kruchinin@gmail.com>
2023-02-09 09:59:47 +0300

    Fix for base layout

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 export const BaseLayout: FC<Props> = ({children}) => (
-  <main className="h-screen min-h-screen lg:w-screen lg:max-w-laptop lg:mx-auto lg:py-5 w-full p-5">
+  <main className="h-screen min-h-screen lg:w-screen lg:max-w-laptop lg:mx-auto lg:py-5 w-full p-5 overflow-y-auto">
     {children}
   </main>
 )
