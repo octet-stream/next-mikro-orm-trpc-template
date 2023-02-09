d3843fa8d9404f29c4ad86b5bd3a5bc287d50233
Nick K <nick.kruchinin@gmail.com>
2023-02-09 10:38:01 +0300

    Fix a typo

--- src/component/NoteRemoveDialog/Warning.tsx
+++ src/component/NoteRemoveDialog/Warning.tsx
@@ -3,7 +3,7 @@ import type {FC} from "react"
 export const Warning: FC = () => (
   <div className="border border-orange-500 dark:border-orange-700 bg-orange-100 dark:bg-orange-200 p-2 rounded-md text-center text-orange-700">
     <span>
-      Warning: This peration cannot be <strong>reverted</strong>!
+      Warning: This operation cannot be <strong>reverted</strong>!
     </span>
   </div>
 )
